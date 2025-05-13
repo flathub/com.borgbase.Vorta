199d5a4b2cde40b527bfb05fce29846531f4569b
Tim <121321767+deenle@users.noreply.github.com>
2025-05-13 20:38:55 +0200

    Update fusermount-wrapper.sh to utilize fusermount3

--- dependencies/fusermount-wrapper.sh
+++ dependencies/fusermount-wrapper.sh
@@ -10,4 +10,8 @@ if [ -e /proc/self/fd/3 ] && [ 3 != "$_FUSE_COMMFD" ]; then
     FD_ARGS="$FD_ARGS --forward-fd=3"
 fi
 
-exec flatpak-spawn --host --forward-fd=1 --forward-fd=2 $FD_ARGS fusermount "$@"
+if flatpak-spawn --host sh -c "command -v fusermount3" > /dev/null; then
+    exec flatpak-spawn --host --forward-fd=1 --forward-fd=2 $FD_ARGS fusermount3 "$@"
+else
+    exec flatpak-spawn --host --forward-fd=1 --forward-fd=2 $FD_ARGS fusermount "$@"
+fi
